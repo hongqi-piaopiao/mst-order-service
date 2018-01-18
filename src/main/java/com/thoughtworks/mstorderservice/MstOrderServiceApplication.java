3df094359bb4adb53dfbd75fda9a4086b4afce48
Fan Mengtian <mengtian.fan@gmail.com>
2018-01-18 19:31:19 +0800

    [tt] #N/A add config for consul

--- src/main/java/com/thoughtworks/mstorderservice/MstOrderServiceApplication.java
+++ src/main/java/com/thoughtworks/mstorderservice/MstOrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.thoughtworks.mstorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MstOrderServiceApplication {
 
 	public static void main(String[] args) {
